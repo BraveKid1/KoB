bd89d7bec79b762fecaf6128934fff8e86fc77c7
muyu <2394728577@qq.com>
2023-07-03 22:26:56 +0800

    4.3 配置Mysql与注册登录模块 下

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,44 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
 
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4Y2U2MWM2MWFiMTQ0ZWY0YjdjMjkzYWFmMjJiNzE5ZSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY4ODM3NTI1NywiZXhwIjoxNjg5NTg0ODU3fQ.1FFJjnlNxUrVXFpH8Ppa09LlmSH6ir-7m9LDOxRJwjg"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "bb",
+    //     password: "pbb",
+    //     confirmPassword: "pbb",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
